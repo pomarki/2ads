49903371cf46f9cdbdf906e1ed14fa588f25a551
Denis Shalaev <pomarki@narod.ru>
2022-08-06 11:52:49 +0300

    03

--- scripts/index.js
+++ scripts/index.js
@@ -2,35 +2,42 @@ const mainScreen = document.getElementById("main-screen");
 const mixButton = document.getElementById("mix-button");
 const sortButton = document.getElementById("sort-button");
 
-ctx = mainScreen.getContext("2d");
+context = mainScreen.getContext("2d");
 
 mainScreen.width = 970;
 mainScreen.height = 301;
 
 let referenceArray = [];
-let mainArr = [];
 
-const getMainArr = () => {
-  for (let i = 1; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
     referenceArray.push(i);
   }
 
+let mainArr = [];
+
+const getMainArr = () => {
+
+  let currentReferenceArr = referenceArray.slice();
   for (let i = 1; i <= 100; i++) {
-    let arrLength = referenceArray.length;
+    let arrLength = currentReferenceArr.length;
     let position = Math.floor(Math.random() * arrLength);
-    let newItem = referenceArray.splice(position, 1)[0];
+    let newItem = currentReferenceArr.splice(position, 1)[0];
     mainArr.push(newItem);
   }
-
+  
   return mainArr;
 };
 
 const renderRow = () => {
+
   getMainArr();
+  context.clearRect(0, 0, mainScreen.width, mainScreen.height);
   for (let i = 0; i <= mainArr.length - 1; i++) {
-    ctx.fillStyle = "white";
-    ctx.fillRect(i * 10, 10, 3, mainArr[i] * 3);
+    context.fillStyle = "white";
+    context.fillRect(i * 9 + 40, 10, 3, mainArr[i] * 2);
   }
+  mainArr.splice(0,100);
+  
 };
 
 mixButton.addEventListener("click", () => renderRow());
