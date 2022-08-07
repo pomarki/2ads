8f2975ff886359a9a66ad9392e1da94ec4f9b4d7
Denis Shalaev <pomarki@narod.ru>
2022-08-07 19:43:56 +0300

    05

--- scripts/bubble.js
+++ scripts/bubble.js
@@ -1,5 +1,8 @@
 let initialArr = [3, 38, 5, 44, 15, 36, 26, 27, 2, 46, 47, 4, 19, 50, 48];
+
+
 let iterations = 0;
+
 const checkRow = (arr) => {
   for (i = 0; i <= arr.length - 1; i++) {
     if (arr[i] > arr[i + 1]) {
@@ -13,11 +16,12 @@ const sortRow = (arr) => {
   let result = arr.slice();
 
   while (!checkRow(result)) {
+    iterations++;
     for (i = 0; i <= result.length - 2; i++) {
-      iterations++;
       if (result[i] > result[i + 1])
         result.splice(i, 2, result[i + 1], result[i]);
     }
+    console.log(result);
   }
 
   return result;
