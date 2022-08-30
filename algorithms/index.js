ff9ce7f5b3ccf954751e5941c21b6c501ef7f8af
Denis Shalaev <pomarki@narod.ru>
2022-08-30 14:01:57 +0300

    29

---
+++ algorithms/index.js
@@ -0,0 +1,5 @@
+import { bogoSort } from "./bogosort.js";
+import { bubbleSort } from "./bubble.js";
+import { shakerSort } from "./shaker.js";
+
+export { bogoSort, bubbleSort, shakerSort };
--- scripts/data.js
+++ scripts/data.js
@@ -1,7 +1,6 @@
 import { getReferenceArr } from "./utils.js";
-import { bubbleSort } from "./bubble.js";
-import { bogoSort } from "./bogosort.js";
-import { shakerSort } from "./shaker.js";
+
+import { bogoSort, bubbleSort, shakerSort } from "../algorithms/index.js";
 const colorArr = ["#ff595e", "#ffca3a", "#8ac926", "#1982c4", "#6a4c93"];
 const mainScreen = document.getElementById("main-screen");
 const mixButton = document.getElementById("mix-button");
@@ -17,15 +16,15 @@ mainScreen.height = 301;
 const PAUSE_DURATION = 150;
 
 const initialCards = [
-  {id: 1, title: "пузырек", method: bubbleSort, weight: 1},
-  {id: 2, title: "шейкер", method: shakerSort, weight: 2},
-  {id: 3, title: "расческа", method: bubbleSort, weight: 1},
-  {id: 4, title: "вставка", method: bubbleSort, weight: 1},
-  {id: 5, title: "выбор", method: bubbleSort, weight: 1},
-  {id: 6, title: "быстрая", method: bubbleSort, weight: 1},
-  {id: 7, title: "слияние", method: bubbleSort, weight: 1},
-  {id: 8, title: "пирамидальная", method: bubbleSort, weight: 1},
-  {id: 9, title: "богосорт", method: bogoSort, weight: 1},
+  { id: 1, title: "пузырек", method: bubbleSort, weight: 1 },
+  { id: 2, title: "шейкер", method: shakerSort, weight: 2 },
+  { id: 3, title: "расческа", method: bubbleSort, weight: 1 },
+  { id: 4, title: "вставка", method: bubbleSort, weight: 1 },
+  { id: 5, title: "выбор", method: bubbleSort, weight: 1 },
+  { id: 6, title: "быстрая", method: bubbleSort, weight: 1 },
+  { id: 7, title: "слияние", method: bubbleSort, weight: 1 },
+  { id: 8, title: "пирамидальная", method: bubbleSort, weight: 1 },
+  { id: 9, title: "богосорт", method: bogoSort, weight: 1 },
 ];
 
 export {
